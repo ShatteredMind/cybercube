45486d740d81dfc97ce7121fcbf3578f2404515b
Alexander Bushlanov <sbrhen@yandex.ru>
2020-11-03 20:28:05 +0500

    Меню

---
+++ Assets/Crap/UIstuff/UI scripts/ButtonHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour
+{    
+   TabSelector tab;
+
+    private void Start()
+    {
+        tab = GameObject.Find("Canvas").GetComponent<TabSelector>();
+    }
+
+    public void ClickOnButton()
+    {
+            //SoundM.PlaySound(0);
+           tab.OpenTab(this.name);
+        
+    }       
+}
